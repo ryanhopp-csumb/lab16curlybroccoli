3f314d9cda31b6eed75b7e24ab0d6b5809d2d549
rhopper2021 <rhopper2021@hightechhigh.org>
2023-10-23 17:39:49 -0700

    commiting both of the files to git

--- myfile.py
+++ myfile.py
@@ -1,4 +1,6 @@
 #Ryan Hopper, 10/23/2023
 #Lab 16, CST 205
-#Finished task 1, almost finished task 2
+#Finished task 1, task 2
+from theotherone import other_list_words
+
 print('hello')
---
+++ theotherone.py
@@ -0,0 +1,7 @@
+#Ryan Hopper, 10/23/2023
+#Lab 16, CST 205
+from __feature__ import snake_case
+
+other_list_words = ['This', 'is', 'really', 'epic']
+
+print (other_list_words)
